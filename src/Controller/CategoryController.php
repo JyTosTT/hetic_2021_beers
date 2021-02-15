d0600025713a447dab8587fbf283147035efed02
JyToSTT <jean.tostivint@gmail.com>
2021-02-15 15:55:20 +0100

    > Fixed twig href

--- src/Controller/CategoryController.php
+++ src/Controller/CategoryController.php
@@ -2,6 +2,7 @@
 
 namespace App\Controller;
 
+use App\Repository\CategoryRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
@@ -9,12 +10,15 @@ use Symfony\Component\Routing\Annotation\Route;
 class CategoryController extends AbstractController
 {
     /**
-     * @Route("/category", name="category")
+     * @Route("/category/{id}", name="category:focus")
+     * @param int $id
+     * @param CategoryRepository $categoryRepository
+     * @return Response
      */
-    public function index(): Response
+    public function categoryFocus(int $id, CategoryRepository $categoryRepository): Response
     {
         return $this->render('category/index.html.twig', [
-            'controller_name' => 'CategoryController',
+            'beer' => $categoryRepository->find($id)
         ]);
     }
 }
