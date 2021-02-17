53fdfd6a3edcfb6ef264b3f73252fe1102cb10b4
JyToSTT <jean.tostivint@gmail.com>
2021-02-17 11:11:35 +0100

    > Adding average

--- src/Twig/AppExtension.php
+++ src/Twig/AppExtension.php
@@ -8,25 +8,39 @@ use Twig\TwigFilter;
 
 class AppExtension extends AbstractExtension
 {
-    public function getFilters()
+    public function getFilters(): array
     {
         return [
-            new TwigFilter('average', [$this, 'average']),
+            new TwigFilter('average', [$this, 'getAverageAndBeerTotal']),
             new TwigFilter('standardDeviation', [$this, 'standardDeviation']),
         ];
     }
 
-    public function average($clients)
+    public function getAverageAndBeerTotal($clients): array
+    {
+        $numberBeerTotal = $this->getNumberBeerTotal($clients);
+        return [
+            'beerTotal' => $numberBeerTotal,
+            'average' => round($numberBeerTotal / count($clients), 2)
+
+        ];
+    }
+
+
+    public function getNumberBeerTotal($clients): int
     {
         $numberBeer = 0;
         foreach ($clients as $client) {
             $numberBeer += $client->getNumberBeer();
         }
-        return round($numberBeer / count($clients), 2);
+        return $numberBeer;
     }
 
-//    public function standardDeviation()
-//    {
-//        return '$price';
-//    }
+
+    public function standardDeviation($clients)
+    {
+        $data = $this->getAverageAndBeerTotal($clients);
+        $sum = 0;
+        return $data['beerTotal'];
+    }
 }
